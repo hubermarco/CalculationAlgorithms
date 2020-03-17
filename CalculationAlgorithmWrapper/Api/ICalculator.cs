f8b7b021f75726e0fa7efe9c9e813fac6fd1984f
Huber, Marco <marco.huber@sivantos.com>
2020-03-17 17:39:31 +0100

    Commenting of ICalculator

--- CalculationAlgorithmWrapper/Api/ICalculator.cs
+++ CalculationAlgorithmWrapper/Api/ICalculator.cs
@@ -2,13 +2,16 @@ namespace CalculatorAlgorithmsWrapper
 {
     public interface ICalculator
     {
-        string SetCalculationString(string calculationString);
         string SetKey(string key);
         string ClearAll();
         string Delete();
-        string Calculate();
         double Calculate(string calculationString);
 
+        // needed for Android Calculator
+        string Calculate();
+
+        // needed for WebApplication
+        string SetCalculationString(string calculationString);
         string CalculateAndReturnString(string calculationString);
     }
 }
