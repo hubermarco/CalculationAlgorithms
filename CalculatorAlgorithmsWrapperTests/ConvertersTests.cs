424e6df38aec93847b31a3dfc324e053deea7b4d
Huber, Marco <marco.huber@sivantos.com>
2020-03-19 16:38:25 +0100

    unit tests added for Converters

---
+++ CalculatorAlgorithmsWrapperTests/ConvertersTests.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorAlgorithmsWrapper;
+using NUnit.Framework;
+
+
+namespace CalculatorAlgorithmsWrapperTests
+{
+    [TestFixture]
+    public class ConvertersTests
+    {
+        [Test]
+        public void When_fix_2_double_was_called_then_corresponding_result_is_returned()
+        {
+            var result = Converters.Fix2Double(27, 5, 2);
+            Assert.AreEqual(-1.25, result);
+        }
+
+        [Test]
+        public void When_fix_2_double_was_called_then_corresponding_result_is_returned_2()
+        {
+            var result = Converters.Fix2Double(27, 6, 0);
+            Assert.AreEqual(27, result);
+        }
+    }
+}
