746f27db3d4ee2c27290bf77dec7b6d696a19520
Huber, Marco <marco.huber@sivantos.com>
2021-04-11 15:46:15 +0200

    Scaling corrected, Unittests added

--- CurveChartImageCreatorTests/ScalingTests.cs
+++ CurveChartImageCreatorTests/ScalingTests.cs
@@ -24,5 +24,28 @@ namespace CurveChartImageCreatorTests
 
             Assert.AreEqual(0, curveOutput.Count, "curveOutput is not supposed to have elements");
         }
+
+        [Test]
+        public void When_curves_have_different_number_of_elements_and_apply_method_is_called_then_an_empty_curve_is_returned()
+        {
+            IList<double> curveOutput1 = new List<double>();
+            IList<double> curveOutput2 = new List<double>();
+            var scalingExponent = 0;
+
+            var curve1 = new List<double> { 1, 2, 3 };
+            var curve2 = new List<double> { 1, 2, 3, 4 };
+
+            Scaling.Apply(
+               curve1: curve1,
+               curve2: curve2,
+               minExponent: 0,
+               maxExponent: 2,
+               curveOutput1: ref curveOutput1,
+               curveOutput2: ref curveOutput2,
+               scalingExponent: ref scalingExponent);
+
+            CollectionAssert.AreEqual(curve1, curveOutput1, "curveOutput1 NOT equal to curve1");
+            CollectionAssert.AreEqual(curve2, curveOutput2, "curveOutput2 NOT equal to curve2");
+        }
     }
 }
