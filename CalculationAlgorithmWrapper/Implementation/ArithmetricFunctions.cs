8086525178187792d2c851890030af26743525cd
Marco Huber <huber.marco@freenet.de>
2023-05-29 20:34:45 +0200

    ArithmetricFunctions refactored

--- CalculationAlgorithmWrapper/Implementation/ArithmetricFunctions.cs
+++ CalculationAlgorithmWrapper/Implementation/ArithmetricFunctions.cs
@@ -1,57 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CalculationAlgorithmWrapper
 {
     internal class ArithmetricFunctions
     {
-        internal static double Fix2Double(IList<double> inputList)
-        {
-            var returnValue = 0.0;
-            if (inputList.Count == 3)
-            {
-                returnValue = Converters.Fix2Double((uint)inputList[0], (int)inputList[1], (int)inputList[2]);
-            }
-            return returnValue;
-        }
-
-        internal static double Double2Fix(IList<double> inputList)
-        {
-            var returnValue = 0.0;
-            if (inputList.Count == 3)
-            {
-                returnValue = Converters.Double2Fix(inputList[0], (int)inputList[1], (int)inputList[2]);
-            }
-            return returnValue;
-        }
-
-        internal static double Double2Bin(IList<double> inputList)
-        {
-            var returnValue = 0.0;
-            if (inputList.Count == 3)
-            {
-                returnValue = Converters.Double2Bin(inputList[0], (int)inputList[1], (int)inputList[2]);
-            }
-            return returnValue;
-        }
-
-        internal static double Bin2Double(IList<double> inputList)
-        {
-            var returnValue = 0.0;
-            if (inputList.Count == 3)
-            {
-                returnValue = Converters.Bin2Double((ulong)inputList[0], (int)inputList[1], (int)inputList[2]);
-            }
-            return returnValue;
-        }
-
-        internal static double Sum(IList<double> inputList)
-        {
-            var sum = 0.0;
-            foreach (var input in inputList)
-            {
-                sum += input;
-            }
-            return sum;
-        }
+        internal static double Fix2Double(IList<double> inputList) => 
+            (inputList.Count == 3) ? Converters.Fix2Double((uint)inputList[0], (int)inputList[1], (int)inputList[2]) : 0;
+       
+        internal static double Double2Fix(IList<double> inputList) => 
+            (inputList.Count == 3) ? Converters.Double2Fix(inputList[0], (int)inputList[1], (int)inputList[2]) : 0;
+        
+        internal static double Double2Bin(IList<double> inputList) => 
+            (inputList.Count == 3) ? Converters.Double2Bin(inputList[0], (int)inputList[1], (int)inputList[2]) : 0;
+        
+        internal static double Bin2Double(IList<double> inputList) => 
+            (inputList.Count == 3) ? Converters.Bin2Double((ulong)inputList[0], (int)inputList[1], (int)inputList[2]) : 0;
+       
+        internal static double Sum(IList<double> inputList) => inputList.Aggregate((total, next) => total + next);
     }
 }
