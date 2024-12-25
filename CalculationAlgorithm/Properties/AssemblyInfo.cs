70746a48ad33416823bf11784c76ca3c1b0ac104
Marco Huber <huber.marco@freenet.de>
2024-12-25 09:10:07 +0100

    Revert "CalculationAlgorithms projects adapted to new SDK projects, WebApplication project adapted to run on VS2022"
    This reverts commit 39b2c61b1bc8d30038d63d748b2187e15ed2b88c.

--- CalculationAlgorithm/Properties/AssemblyInfo.cs
+++ CalculationAlgorithm/Properties/AssemblyInfo.cs
@@ -5,7 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
+[assembly: AssemblyTitle("CalculationAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculationAlgorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("CalculationAlgorithmTests")]
--- CalculationAlgorithmWrapper/Properties/AssemblyInfo.cs
+++ CalculationAlgorithmWrapper/Properties/AssemblyInfo.cs
@@ -5,8 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
+[assembly: AssemblyTitle("CalculationAlgorithmWrapper")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculationAlgorithmWrapper")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,6 +29,10 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("CalculationAlgorithmWrapperTests")]
